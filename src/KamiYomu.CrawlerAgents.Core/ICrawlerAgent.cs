45f12e16a870185ffbb115a48b396ae2e9a34686
Marcos Costa <marcos.lcosta@live.com>
2026-08-24 18:43:32 -0400

    add download headers as interface

--- src/KamiYomu.CrawlerAgents.Core/ICrawlerAgent.cs
+++ src/KamiYomu.CrawlerAgents.Core/ICrawlerAgent.cs
@@ -46,10 +46,4 @@ public interface ICrawlerAgent : IDisposable
     /// <param name="cancellationToken">Optional token to cancel the operation.</param>
     /// <returns>A collection of <see cref="Page"/> objects representing individual chapter pages.</returns>
     Task<IEnumerable<Page>> GetChapterPagesAsync(Chapter chapter, CancellationToken cancellationToken);
-    /// <summary>
-    /// Retrieves the default HTTP headers used by the crawler agent in its requests to the target site.
-    /// The download of files will use these headers to ensure proper authentication, content negotiation, and other necessary request configurations.
-    /// </summary>
-    /// <returns>A collection of key-value pairs representing the default headers.</returns>
-    IEnumerable<KeyValuePair<string, string>> GetDefaultHeaders();
 }

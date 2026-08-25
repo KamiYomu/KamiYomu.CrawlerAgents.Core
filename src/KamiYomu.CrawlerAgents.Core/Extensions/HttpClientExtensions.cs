5ca186356e5995506f08a103dc879c903cc541e2
Marcos Costa <marcos.lcosta@live.com>
2026-08-24 20:20:23 -0400

    Add extensions and improve documentation

--- src/KamiYomu.CrawlerAgents.Core/Extensions/HttpClientExtensions.cs
+++ src/KamiYomu.CrawlerAgents.Core/Extensions/HttpClientExtensions.cs
@@ -1,17 +1,28 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace KamiYomu.CrawlerAgents.Core.Extensions;
 
 /// <summary>
-/// httpclient extensions
+/// Provides extension methods for configuring HTTP headers on <see cref="HttpClient"/>
+/// and <see cref="HttpRequestMessage"/> instances.
 /// </summary>
+/// <remarks>
+/// These extensions simplify the process of adding multiple HTTP headers without validation,
+/// useful for custom or non-standard headers that may not pass standard HTTP header validation rules.
+/// </remarks>
 public static class HttpClientExtensions
 {
     /// <summary>
-    /// Loads the specified header into the HttpRequestMessage headers.
+    /// Adds multiple HTTP headers to an <see cref="HttpRequestMessage"/> without validation.
     /// </summary>
-    /// <param name="request">The HttpRequestMessage to load the header into.</param>
-    /// <param name="headers">The headers to load.</param>
+    /// <param name="request">The <see cref="HttpRequestMessage"/> to add headers to.</param>
+    /// <param name="headers">An enumerable collection of key-value pairs representing header names and values.</param>
+    /// <remarks>
+    /// Headers are added using <see cref="HttpHeaders.TryAddWithoutValidation(string, string)"/>,
+    /// which allows for custom or non-standard headers that may not conform to RFC specifications.
+    /// If a header fails to be added, the operation continues silently.
+    /// </remarks>
     public static void AddRangeHeaders(this HttpRequestMessage request, params KeyValuePair<string, string>[] headers)
     {
         foreach (KeyValuePair<string, string> header in headers)
@@ -21,10 +32,16 @@ public static class HttpClientExtensions
     }
 
     /// <summary>
-    /// Loads the specified header into the HttpClient default request headers.
+    /// Adds multiple HTTP headers to an <see cref="HttpClient"/>'s default request headers without validation.
     /// </summary>
-    /// <param name="httpClient">The HttpClient to load the header into.</param>
-    /// <param name="headers">The headers to load.</param>
+    /// <param name="httpClient">The <see cref="HttpClient"/> instance to configure.</param>
+    /// <param name="headers">An enumerable collection of key-value pairs representing header names and values.</param>
+    /// <remarks>
+    /// Headers are added to the <see cref="HttpClient.DefaultRequestHeaders"/> using <see cref="HttpHeaders.TryAddWithoutValidation(string, string)"/>,
+    /// allowing for custom or non-standard headers that may not conform to RFC specifications.
+    /// These headers will be included in all subsequent requests made with this <see cref="HttpClient"/> instance.
+    /// If a header fails to be added, the operation continues silently.
+    /// </remarks>
     public static void AddRangeHeaders(this HttpClient httpClient, params KeyValuePair<string, string>[] headers)
     {
         foreach (KeyValuePair<string, string> header in headers)
